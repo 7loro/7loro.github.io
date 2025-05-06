3160ffd2493aea7221c9cd64b060198613b02cec
Casper <nowfeel88@gmail.com>
2025-05-07 01:05:54 +0900

    Fix markdown header font size

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <h1>About 헤더1</h1>
+      <h2>About 헤더2</h2>
+      <h3>About 헤더3</h3>
       <p>This is the about page.</p>
     </>
   );
